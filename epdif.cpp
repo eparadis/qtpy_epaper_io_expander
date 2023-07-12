15c5f72cdfcf23c919557e6b0d74b61e6e4a7ac6
Ed Paradis <eparadis@users.noreply.github.com>
2023-07-12 01:05:10 -0700

    bit bang the SPI the correct endianness derrrp

--- epdif.cpp
+++ epdif.cpp
@@ -29,22 +29,25 @@ void EpdIf::DelayMs(unsigned int delaytime) {
 }
 
 void EpdIf::SpiTransfer(unsigned char data) {
+  mcp.digitalWrite(MCP_DEBUG_LED, HIGH);
   mcp.digitalWrite(CS_PIN, LOW);
   // shift out a byte
   for(int i = 0; i < 8; i += 1) {
     // set data
-    mcp.digitalWrite(SPI_DATA, bitRead(data, i));
+    mcp.digitalWrite(SPI_DATA, data & 0x80);
+    data = data << 1;
     // clock high then low
     mcp.digitalWrite(SPI_CLK, HIGH);
     // possible delay here, but things are probably so slow coming out of the IO expander we don't need it
     mcp.digitalWrite(SPI_CLK, LOW);
   }
   mcp.digitalWrite(CS_PIN, HIGH);
+  mcp.digitalWrite(MCP_DEBUG_LED, LOW);
 }
 
 int EpdIf::IfInit(void) {
   if (!mcp.begin_I2C()) {
-    return 1; // could not init I2C comms to IO expander
+    return -1; // could not init I2C comms to IO expander
   }
   mcp.pinMode(CS_PIN, OUTPUT);
   mcp.pinMode(RST_PIN, OUTPUT);
@@ -52,6 +55,8 @@ int EpdIf::IfInit(void) {
   mcp.pinMode(BUSY_PIN, INPUT);
   mcp.pinMode(SPI_DATA, OUTPUT);
   mcp.pinMode(SPI_CLK, OUTPUT);
+
+  mcp.pinMode(MCP_DEBUG_LED, OUTPUT);
   return 0;
 }
 
