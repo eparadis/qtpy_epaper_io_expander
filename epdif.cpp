2bf1aaadfd29694c3617d2067e6e850d8c3947ce
Ed Paradis <eparadis@users.noreply.github.com>
2023-07-11 19:49:39 -0700

    use IO expander. compiles and downlaods

--- epdif.cpp
+++ epdif.cpp
@@ -2,11 +2,13 @@
 Original by EastRising Technology Co.,LTD
 Web: http://www.buydisplay.com
 
-
+2023-07-11 edp - Modified to use MCP23X17 IO expander
 */
 
 #include "epdif.h"
-// #include <spi.h>
+#include <Adafruit_MCP23X17.h>
+
+Adafruit_MCP23X17 mcp;
 
 EpdIf::EpdIf() {
 };
@@ -15,30 +17,41 @@ EpdIf::~EpdIf() {
 };
 
 void EpdIf::DigitalWrite(int pin, int value) {
-    // digitalWrite(pin, value);
+  mcp.digitalWrite(pin, value);
 }
 
 int EpdIf::DigitalRead(int pin) {
-    // return digitalRead(pin);
+  return mcp.digitalRead(pin);
 }
 
 void EpdIf::DelayMs(unsigned int delaytime) {
-    // delay(delaytime);
+  delay(delaytime);
 }
 
 void EpdIf::SpiTransfer(unsigned char data) {
-    // digitalWrite(CS_PIN, LOW);
-    // SPI.transfer(data);
-    // digitalWrite(CS_PIN, HIGH);
+  mcp.digitalWrite(CS_PIN, LOW);
+  // shift out a byte
+  for(int i = 0; i < 8; i += 1) {
+    // set data
+    mcp.digitalWrite(SPI_DATA, bitRead(data, i));
+    // clock high then low
+    mcp.digitalWrite(SPI_CLK, HIGH);
+    // possible delay here, but things are probably so slow coming out of the IO expander we don't need it
+    mcp.digitalWrite(SPI_CLK, LOW);
+  }
+  mcp.digitalWrite(CS_PIN, HIGH);
 }
 
 int EpdIf::IfInit(void) {
-    // pinMode(CS_PIN, OUTPUT);
-    // pinMode(RST_PIN, OUTPUT);
-    // pinMode(DC_PIN, OUTPUT);
-    // pinMode(BUSY_PIN, INPUT); 
-    // SPI.beginTransaction(SPISettings(2000000, MSBFIRST, SPI_MODE0));
-    // SPI.begin();
-    return 0;
+  if (!mcp.begin_I2C()) {
+    return 1; // could not init I2C comms to IO expander
+  }
+  mcp.pinMode(CS_PIN, OUTPUT);
+  mcp.pinMode(RST_PIN, OUTPUT);
+  mcp.pinMode(DC_PIN, OUTPUT);
+  mcp.pinMode(BUSY_PIN, INPUT);
+  mcp.pinMode(SPI_DATA, OUTPUT);
+  mcp.pinMode(SPI_CLK, OUTPUT);
+  return 0;
 }
 
--- epdif.h
+++ epdif.h
@@ -9,12 +9,18 @@ Web: http://www.buydisplay.com
 #define EPDIF_H
 
 #include <arduino.h>
+#include <Adafruit_MCP23X17.h>
 
-// Pin definition
-#define RST_PIN         2
+
+// Pin definitions. These are on the MCP23017
+// 0..7  = PA0..7
+// 8..15 = PB0..7
+#define RST_PIN         0
 #define DC_PIN          1
-#define CS_PIN          0
-#define BUSY_PIN        3
+#define CS_PIN          2
+#define SPI_DATA        3
+#define SPI_CLK         4
+#define BUSY_PIN        5     // the only input!
 
 class EpdIf {
 public:
@@ -26,6 +32,9 @@ public:
     static int  DigitalRead(int pin);
     static void DelayMs(unsigned int delaytime);
     static void SpiTransfer(unsigned char data);
+
+private:
+
 };
 
 #endif
